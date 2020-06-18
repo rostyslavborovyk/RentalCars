d5c3e69e6851c9f7b0cf0f54c0d32431c93346d9
rostislav <rostmoguchiy@gmail.com>
2020-06-18 21:31:33 +0300

    fix: fixed tests

--- quart_backend/app/test_app.py
+++ quart_backend/app/test_app.py
@@ -12,8 +12,8 @@ def _test_app():
 @pytest.mark.asyncio
 async def test_empty_get_healthcheck(test_app):
     test_client = test_app.test_client()
-    response = await test_client.get('/healthcheck')
 
+    response = await test_client.get('/healthcheck')
     assert response.status_code == 200
 
     data = await response.get_data()
@@ -23,42 +23,10 @@ async def test_empty_get_healthcheck(test_app):
 @pytest.mark.asyncio
 async def test_db_healthcheck(test_app):
     test_client = test_app.test_client()
-    response = await test_client.get('/db_healthcheck')
 
+    response = await test_client.get('/db_healthcheck')
     assert response.status_code == 200
 
     data = await response.get_data()
     assert data.decode("utf-8") == "Pavel"
 
-
-@pytest.mark.asyncio
-async def test_clients_bp_healthcheck(test_app):
-    test_client = test_app.test_client()
-    response = await test_client.get('/clients/healthcheck')
-
-    assert response.status_code == 200
-
-    data = await response.get_data()
-    assert data.decode("utf-8") == "Ok"
-
-
-@pytest.mark.asyncio
-async def test_orders_bp_healthcheck(test_app):
-    test_client = test_app.test_client()
-    response = await test_client.get('/orders/healthcheck')
-
-    assert response.status_code == 200
-
-    data = await response.get_data()
-    assert data.decode("utf-8") == "Ok"
-
-
-@pytest.mark.asyncio
-async def test_cars_bp_healthcheck(test_app):
-    test_client = test_app.test_client()
-    response = await test_client.get('/cars/healthcheck')
-
-    assert response.status_code == 200
-
-    data = await response.get_data()
-    assert data.decode("utf-8") == "Ok"
